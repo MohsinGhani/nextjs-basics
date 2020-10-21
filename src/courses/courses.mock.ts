5266f792efb695059040ab9cbe0eaa6bc1032a59
mohsinghani <mohsinghani.777@gmail.com>
2020-10-21 18:30:51 +0500

    develop nestjs basic example

---
+++ src/courses/courses.mock.ts
@@ -0,0 +1,5 @@
+export const COURSES = [
+    { id: 1, title: 'ReactJS', description: 'this is the ReactJS course', author: 'Moshin', url: 'www.reactjs.com' },
+    { id: 2, title: 'NextJS', description: 'this is the NextJS course', author: 'Moshin', url: 'www.nextjs.com' },
+    { id: 3, title: 'NestJS', description: 'this is the NestJS course', author: 'Moshin', url: 'www.nestjs.com' }
+]
---
+++ src/courses/courses.service.ts
@@ -0,0 +1,50 @@
+import { HttpException, Injectable, ParseIntPipe } from '@nestjs/common';
+import { COURSES } from './courses.mock';
+@Injectable()
+export class CoursesService {
+    courses = COURSES;
+
+    getCourses(): Promise<any> {
+        return new Promise(resolve => {
+            resolve(this.courses)
+        })
+    }
+
+    getCourse(courseId): Promise<any> {
+        let id = Number(courseId)
+        return new Promise(resolve => {
+            const course = this.courses.find(course => course.id === id);
+
+            if (!course) {
+                throw new HttpException('Course does not exist', 404);
+            }
+
+            resolve(course);
+        })
+    }
+
+    addCourse(course): Promise<any> {
+        return new Promise(resolve => {
+            let newCourse = {
+                ...course,
+                id: parseInt(course.id)
+            }
+            this.courses.push(newCourse)
+            resolve(this.courses)
+        })
+    }
+
+    deleteCourse(courseId): Promise<any> {
+        let id = Number(courseId);
+        return new Promise(resolve => {
+            let index = this.courses.findIndex(course => course.id === id)
+
+            if (index === -1) {
+                throw new HttpException('Course does not exist', 400);
+            }
+
+            this.courses.splice(index, 1);
+            resolve(this.courses)
+        })
+    }
+}
